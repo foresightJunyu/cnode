dd79537a6a7ee1917de57b2b15717d2dcded9235
foresightJunyu <373034891@qq.com>
2021-10-23 16:23:50 +0800

    定制主页路由

---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header/>
+    <PostList/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import Header from '@/components/Header.vue'
+import PostList from '@/components/PostList.vue'
+
+@Component({
+  components: { Header, PostList }
+})
+export default class HomePage extends Vue {
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
