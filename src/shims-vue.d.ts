f55d22dc9cd9eb23e2eabe8da7dcaaf29156d9c3
foresightJunyu <373034891@qq.com>
2021-10-23 16:27:41 +0800

    解决 TS 报错问题

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,23 @@
+import { AxiosStatic, AxiosRequestConfig, AxiosResponse } from 'axios'
+
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
+}
+declare module '*.svg' {
+  const content: string
+  export default content
+}
+declare module 'vue/types/vue' {
+// 3. 声明为 Vue 补充的东西
+  interface Vue {
+    $axios: AxiosStatic
+  }
+}
+// theFileYouDeclaredTheCustomConfigIn.ts
+declare module 'axios' {
+  export interface AxiosRequestConfig {
+    page: number,
+    limit: number
+  }
 }
