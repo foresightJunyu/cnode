a008592dd95a891084e87235c375488471883f39
foresightJunyu <373034891@qq.com>
2021-10-31 23:07:31 +0800

    去除多余中间累赘

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Header/>
+    <div class="main">
+      <router-view name="slideBar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
   </div>
 </template>
+
+
+<script lang="ts">
+
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+import PostList from '@/components/PostList.vue';
+
+@Component({
+  components: {Header, PostList}
+})
+export default class App extends Vue {
+}
+</script>
 
 <style lang="scss">
 @import '~@/assets/style/reset.scss';
